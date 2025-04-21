d55dce9b715f031793b603679d79adbdfbc9b303
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-04-21 17:19:26 +0300

    adding name of the excursion

--- plan_alone.js
+++ plan_alone.js
@@ -1,29 +1,35 @@
+let difference;
 
 function holidayTime() {
     let startDate = new Date(document.querySelector('#start-date').value);
     let endDate = new Date(document.querySelector('#end-date').value);
-    console.log(startDate);
-    console.log(endDate);
 
-    difference = endDate - startDate;
-    console.log(difference);
+    difference = (endDate - startDate) / (1000 * 60 * 60 * 24);
 
+    return difference;
 }
 
-
-
 document.getElementById('region').addEventListener('change', region);
+
 function region() {
     let selectedRegion = '';
-
+    let regionName = '';
     switch(selectedRegion) {
         case 'Northwest':
+            regionName = 'Северозападна България';
             break;
         case 'Northeast':
+            regionName = 'Североизточна България';
             break;
         case 'Southwest':
+            regionName = 'Югозападна България';
             break;
         case 'Southwest':
+            regionName = 'Югоизточна България';
             break;
     }
+
+    console.log(regionName);
+
+    document.getElementById('excursion-name').innerText = `${difference}-дневна екскурзия в ${regionName}`;
 };
