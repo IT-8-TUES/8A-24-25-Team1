02c89ddedca067dec203a9682d28b0fda1b642b5
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-04-21 15:35:08 +0300

    adding switch in the JS file for the selector

--- plan_alone.js
+++ plan_alone.js
@@ -1,23 +1,19 @@
 document.getElementById('region').addEventListener('change', region);
 
 function region() {
-    const selectedCity = this.value;
-    const output1 = document.getElementById('output1');
-  
-    switch (selectedCity) {
-      case 'option1':
-        output1.textContent = 'Северозападна Бълагрия';
-        break;
-      case 'option2':
-        output1.textContent = 'Североизточна България';
-        break;
-      case 'option3':
-        output1.textContent = 'Югозападна България';
-        break;
-      case 'option4':
-        output1.textContent = 'Югоизточна България';
-        break;
-      default:
-        output1.textContent = '';
+    let selectedRegion = '';
+
+    switch(selectedRegion) {
+        case 'Northwest':
+            break;
+        case 'Northeast':
+            break;
+        case 'Southwest':
+            break;
+        case 'Southwest':
+            break;
     }
-  });
+
+    console.log(selectedRegion);
+
+};
