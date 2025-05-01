0cab35d456de121cf203fd85934eccd6a48a6d43
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-05-01 18:04:08 +0300

    adding limit of days and alerts if the input is not right

--- plan_alone.js
+++ plan_alone.js
@@ -1,11 +1,15 @@
-let difference;
+let difference = 0;
 
 function holidayTime() {
     let startDate = new Date(document.querySelector('#start-date').value);
     let endDate = new Date(document.querySelector('#end-date').value);
 
+    
     difference = (endDate - startDate) / (1000 * 60 * 60 * 24);
-
+    if(difference > 10 || difference < 1) {
+        alert('Почивката тябва да е най-малко 1 ден и най-много 10 дена');
+        difference = 0;
+    }
     return difference;
 }
 
@@ -26,9 +30,14 @@ function region() {
         case 'Southeast':
             regionName = 'Югоизточна България';
             break;
+        case '':
+            regionName = null;
+            alert('Изберете регион');
+            break;
     }
 
-    console.log(regionName)
-
-    document.getElementById('excursion-name').innerText = `${difference}-дневна екскурзия в ${regionName}`;
-};
+    console.log(regionName);
+    if(difference > 0 && regionName !== null) {
+        document.getElementById('excursion-name').innerText = `${difference}-дневна екскурзия в ${regionName}`;
+    }
+}
