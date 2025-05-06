557529f83af271bc9ab79d0abc85732ab457e450
Eleonora Dimitrova <elli.d.dimitrova@gmail.com>
2025-05-06 17:05:12 +0300

    adding function for defining the sights

--- plan_alone.js
+++ plan_alone.js
@@ -42,7 +42,7 @@ function region() {
         document.getElementById('excursion-name').innerText = `${difference}-дневна екскурзия в ${regionName}`;
         showingDays();
         towns(regionName);
-        selectorTowns();
+        displayTowns();
      //   namingDays();
     }
 
@@ -59,92 +59,173 @@ function showingDays() {
     
 }
 
-let town1 = '', town2 = '', town3 = '', town4 = '', town5 = '';
+let townList = [];
 
 // Функция за определяне на градовете спрямо региона
 
 function towns(regionName) {
     switch(regionName) {
         case 'Северозападна България':
-            town1 = 'Видин';
-            town2 = 'Белоградчик';
-            town3 = 'Берковица';
-            town4 = 'Чипровци';
-            town5 = 'Козлодуй';
+            townList = ['Видин', 'Белоградчик', 'Берковица', 'Чипровци', 'Козлодуй'];
             break;
         case 'Североизточна България':
-            town1 = 'Шумен';
-            town2 = 'Варна';
-            town3 = 'Велико Търново';
-            town4 = 'Силистра';
-            town5 = 'Добрич';
+            townList = ['Шумен', 'Варна', 'Велико Търново', 'Силистра', 'Добрич'];
             break;
         case 'Югозападна България':
-            town1 = 'Мелник';
-            town2 = 'Банско';
-            town3 = 'Сандански';
-            town4 = 'Кюстендил';
-            town5 = 'София';
+            townList = ['Мелник', 'Банско', 'Сандански', 'Кюстендил', 'София'];
             break;
         case 'Югоизточна България':
-            town1 = 'Бургас';
-            town2 = 'Стара Загора';
-            town3 = 'Хасково';
-            town4 = 'Сливен';
-            town5 = 'Несебър';
+            townList = ['Бургас', 'Стара Загора', 'Хасково', 'Сливен', 'Несебър'];
             break;
     }
 }
 
-function selectorTowns() {
+//Функция за показване на градовете като опции
+
+function displayTowns() {
+    let i;
     
+    for(i = 0; i < 3; i++) {
+        document.querySelectorAll(`.town-option${i+1}`).forEach(
+            function(item) {
+                item.innerText = townList[i];
+                item.value = townList[i];
+            })
+    }
 }
-/* Функция за именуване на дните
 
-function namingDays() {
-    document.getElementById('day1-name').innerText = `Ден 1 - ${town1}`;
-    if(difference > 5) {
-        document.getElementById('day2-name').innerText = `Ден 2 - ${town1}`;
-        document.getElementById('day3-name').innerText = `Ден 3 - ${town2}`;
-        if(difference > 6) {
-            document.getElementById('day4-name').innerText = `Ден 4 - ${town2}`;
-            document.getElementById('day5-name').innerText = `Ден 5 - ${town3}`;
-            if(difference > 7) {
-                document.getElementById('day6-name').innerText = `Ден 6 - ${town3}`;
-                document.getElementById('day7-name').innerText = `Ден 7 - ${town4}`;
-                if(difference > 8) {
-                    document.getElementById('day8-name').innerText = `Ден 8 - ${town4}`;
-                    document.getElementById('day9-name').innerText = `Ден 9 - ${town5}`;
-                    if(difference > 9) {
-                        document.getElementById('day10-name').innerText = `Ден 10 - ${town5}`;
-                    }
-                }
-                else {
-                    document.getElementById('day8-name').innerText = `Ден 8 - ${town5}`;
-                }
+
+let sights = {
+    sight1: '',
+    sight2: '',
+    sight3: '',
+};
+
+//Функция за избиране на забележителности
+
+function defineSights() {
+    let i;
+    for(i = 0; i <= difference; i++) {
+        selectorTownValue = document.querySelector(`.town-option${i+1}`).value;
+
+        if(townList = ['Видин', 'Белоградчик', 'Берковица', 'Чипровци', 'Козлодуй']) {
+            switch(selectorTownValue) {
+                case 'town1':
+                    sights.sight1 = 'Крепостта Баба Вида',
+                    sights.sight2 = 'Катедрален храм "Св. Димитър"',
+                    sights.sight3 = 'Дунавският парк';
+                    break;
+                case 'town2': 
+                    sights.sight1 = 'Белоградчишките скали',
+                    sights.sight2 = 'Белоградчишката крепост (Калето',
+                    sights.sight3 = 'Пещера Магура';
+                    break;
+                case 'town3': 
+                    sights.sight1 = 'Часовникова кула',
+                    sights.sight2 = 'Връх Ком',
+                    sights.sight3 = 'Клисурски манастир';
+                    break;
+                case 'town4': 
+                    sights.sight1 = 'Чипровският манастир "Св. Иван Рилски"',
+                    sights.sight2 = 'Исторически музей – Чипровци',
+                    sights.sight3 = 'Чипровски водопад';
+                    break;
+                case 'town5': 
+                    sights.sight1 = 'Национален музей-параход "Радецки"',
+                    sights.sight2 = 'Исторически музей Козлодуй',
+                    sights.sight3 = 'Паметник на Христо Ботев и четата му';
+                    break;
             }
-            else {
-                document.getElementById('day6-name').innerText = `Ден 5 - ${town4}`;
-                document.getElementById('day7-name').innerText = `Ден 6- ${town5}`;
+        }
+        else if(townList = ['Шумен', 'Варна', 'Велико Търново', 'Силистра', 'Добрич']) {
+            switch(selectorTownValue) {
+                case 'town1':
+                    sights.sight1 = 'Шуменска крепост',
+                    sights.sight2 = 'Паметник "Създатели на българската държава"',
+                    sights.sight3 = 'Мадарски конник';
+                    break;
+                case 'town2': 
+                    sights.sight1 = 'Морската градина',
+                    sights.sight2 = 'Археологически музей Варна',
+                    sights.sight3 = 'Аладжа манастир';
+                    break;
+                case 'town3': 
+                    sights.sight1 = 'Крепост Царевец',
+                    sights.sight2 = 'Самоводска чаршия',
+                    sights.sight3 = 'Архитектурен резерват Арбанаси';
+                    break;
+                case 'town4': 
+                    sights.sight1 = 'Биосферен резерват "Сребърна"',
+                    sights.sight2 = ' Крепост Дуросторум–Дръстър',
+                    sights.sight3 = 'Римската гробница';
+                    break;
+                case 'town5': 
+                    sights.sight1 = 'Етнографски комплекс "Стария Добрич"',
+                    sights.sight2 = 'Градски парк "Св. Георги"',
+                    sights.sight3 = 'Музей "Йовковият Добрич"' ;
+                    break;
             }
         }
-        else {
-            document.getElementById('day4-name').innerText = `Ден 4 - ${town3}`;
-            document.getElementById('day5-name').innerText = `Ден 5 - ${town4}`;
-            document.getElementById('day6-name').innerText = `Ден 6 - ${town5}`;
+        else if(townList = ['Мелник', 'Банско', 'Сандански', 'Кюстендил', 'София']) {
+            switch(selectorTownValue) {
+                case 'town1':
+                    sights.sight1 = 'Мелнишките пирамиди',
+                    sights.sight2 = 'Кордопуловата къща',
+                    sights.sight3 = 'Роженски манастир';
+                    break;
+                case 'town2': 
+                    sights.sight1 = 'Възрожденски къщи',
+                    sights.sight2 = 'Църквата "Света Троица"',
+                    sights.sight3 = 'Пирин';
+                    break;
+                case 'town3': 
+                    sights.sight1 = 'Археологически музей с римски мозайки',
+                    sights.sight2 = 'Римски терми',
+                    sights.sight3 = 'Скакавишкия водопад';
+                    break;
+                case 'town4': 
+                    sights.sight1 = 'Крепост Хисарлъка',
+                    sights.sight2 = ' Крепост Дуросторум–Дръстър',
+                    sights.sight3 = 'Римската гробница';
+                    break;
+                case 'town5': 
+                    sights.sight1 = 'Храм-паметник "Свети Александър Невски"',
+                    sights.sight2 = 'Боянска църква',
+                    sights.sight3 = 'Витоша планина – Черни връх' ;
+                    break;
+            }
         }
-        
-    }
-    else {
-        document.getElementById('day2-name').innerText = `Ден 2 - ${town2}`;
-        document.getElementById('day3-name').innerText = `Ден 3 - ${town3}`;
-        document.getElementById('day4-name').innerText = `Ден 4 - ${town4}`;
-        document.getElementById('day5-name').innerText = `Ден 5 - ${town5}`;
-    }
+        else if(townList = ['Бургас', 'Стара Загора', 'Хасково', 'Сливен', 'Несебър']) {
+            switch(selectorTownValue) {
+                case 'town1':
+                    sights.sight1 = 'Морска градина',
+                    sights.sight2 = 'Остров Света Анастасия',
+                    sights.sight3 = 'Природозащитен център "Пода"';
+                    break;
+                case 'town2': 
+                    sights.sight1 = 'Мемориален комплекс "Бранителите на Стара Загора"',
+                    sights.sight2 = 'Античният форум "Августа Траяна"',
+                    sights.sight3 = 'Парк "Аязмото"';
+                    break;
+                case 'town3': 
+                    sights.sight1 = 'Монумент "Света Богородица с Младенеца"',
+                    sights.sight2 = 'Лесопарк "Кенана"',
+                    sights.sight3 = 'Перперикон';
+                    break;
+                case 'town4': 
+                    sights.sight1 = 'Крепост Туида',
+                    sights.sight2 = 'Природен парк "Сините камъни"',
+                    sights.sight3 = 'Пантеон на героите и Хаджи Димитър';
+                    break;
+                case 'town5': 
+                    sights.sight1 = 'Старият Несебър',
+                    sights.sight2 = 'Вятърната мелница',
+                    sights.sight3 = 'Църквата "Свети Стефан"' ;
+                    break;
+            }
         }
-*/
-   
 
-    
-    
+    }
 
+}
+    
